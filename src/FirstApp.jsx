3e99589f73807122105da1e5cc4cb90d8fb3d432
Juan Bautista Padilla <72109896+juanbpadilla@users.noreply.github.com>
2024-10-02 11:39:23 -0400

    Impresión de variables en el HTML

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,9 +1,12 @@
 
+const getMessage = (message) => `Hola, ${message}`
+
 export const FirstApp = () => {
 
   return (
     <>
-      <h1>FirstApp</h1>
+      <h1>{ getMessage('Juan') }</h1>
+      {/* <code>{ JSON.stringify(newMessage) }</code> */ }
       <p>Soy un parrafo</p>
     </>
   )

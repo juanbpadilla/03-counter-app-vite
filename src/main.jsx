9af99f6168e7cb7c3e0edfedd46829de7596f633
Juan Bautista Padilla <72109896+juanbpadilla@users.noreply.github.com>
2024-10-02 11:08:52 -0400

    Tarea: Crear nuevo componenete

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client';
 import { HelloWorldApp } from './HelloWorldApp';
+import { FirstApp } from './FirstApp';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HelloWorldApp />
+    <FirstApp />
   </React.StrictMode>
 )

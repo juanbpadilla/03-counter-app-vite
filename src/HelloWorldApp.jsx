26161d7938b8cadff0ad03ebb65aa7888a352c97
Juan Bautista Padilla <72109896+juanbpadilla@users.noreply.github.com>
2024-10-02 10:59:56 -0400

    Nuestro primer componente

---
+++ src/HelloWorldApp.jsx
@@ -0,0 +1,10 @@
+
+// export function App() {
+//   return <h1>Hola mundo</h1>;
+// }
+
+export const HelloWorldApp = () => {
+  return (
+    <h1>Hello World App</h1>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,9 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client';
-
-function App() {
-  return <h1>Hola mundo</h1>;
-}
+import { HelloWorldApp } from './HelloWorldApp';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <HelloWorldApp />
   </React.StrictMode>
 )

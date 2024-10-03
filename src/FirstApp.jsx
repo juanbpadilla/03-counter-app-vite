9f1221c24a66cfa0b21f55add3cbfde35407bf55
Juan Bautista Padilla <72109896+juanbpadilla@users.noreply.github.com>
2024-10-03 00:40:30 -0400

    DefaultProps

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,7 +1,7 @@
 
 import PropTypes from 'prop-types';
 
-export const FirstApp = ({ title, subTitle }) => {
+export const FirstApp = ({ title, subTitle, name }) => {
 
   // console.log(props)
 
@@ -10,11 +10,33 @@ export const FirstApp = ({ title, subTitle }) => {
       <h1>{ title }</h1>
       {/* <code>{ JSON.stringify(newMessage) }</code> */ }
       <p>{ subTitle }</p>
+      <p>{ name }</p>
     </>
   )
 }
 
+// Los PropTypes son una forma de documentar los componentes, no son obligatorios
+// .. pero son una buena práctica para saber qué tipo de datos se esperan
+// .. y qué valores se deben pasar a un componente
+// Se pueden establecer los tipos de datos que se esperan en las props
+// .. de un componente, para ello se utiliza la propiedad propTypes
+// .. de la función del componente
+// .. Se importa PropTypes de 'prop-types'
+// .. Se establece el tipo de dato que se espera en cada propiedad
+
+// ⚠️ Se recomienda poner los PropTypes al final del archivo
+
 FirstApp.propTypes = {
   title: PropTypes.string.isRequired,
-  subTitle: PropTypes.number
+  subTitle: PropTypes.string,
+  name: PropTypes.string
+}
+
+// .defaultProps Actualmente deprecated, se recomienda establecer 
+// ..los valores por defecto en la función
+// ..FirstApp = ({ title = 'No hay t..', subTitle = 'No hay subt..', name = 'Juan Pe..' }) => {
+FirstApp.defaultProps = {
+  title: 'No hay título',
+  subTitle: 'No hay subtítulo',
+  name: 'Juan Perez'
 }

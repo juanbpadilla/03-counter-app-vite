c9b05aaef82634b554b19920873c2c424b48c0e1
Juan Bautista Padilla <72109896+juanbpadilla@users.noreply.github.com>
2024-10-03 00:55:44 -0400

    Tarea -  Componente CounterApp

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client';
-import { FirstApp } from './FirstApp';
+// import { FirstApp } from './FirstApp';
+import { CounterApp } from './CounterApp';
+
 import './styles.css'
 
 // import { HelloWorldApp } from './HelloWorldApp';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <FirstApp />
+    {/* <FirstApp /> */ }
+    <CounterApp value={ 10 } />
   </React.StrictMode>
 )

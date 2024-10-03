ec88e4cc64fd3961746d3724a69f19dd0f5b70c6
Juan Bautista Padilla <72109896+juanbpadilla@users.noreply.github.com>
2024-10-03 01:24:58 -0400

    Evento click (Eventos en general)

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -1,10 +1,39 @@
 import PropTypes from 'prop-types'
 
+
+// Si la función no necesita acceder a las props, 
+// se puede hacer fuera del componente:
+//    const handleAdd = (event) => {
+//      console.log(event)
+//    }
+
 export const CounterApp = ({ value }) => {
+
+  // function handleAdd(event) { console.log(event) }
+  // function handleAdd(event, newValue) {
+  //   console.log(newValue)
+  // }
+
+  // Como función de flecha
+  // const handleAdd = (event, newValue) => {
+  //   console.log(newValue)
+  // }
+  const handleAdd = (event) => {
+    console.log(event)
+  }
+
   return (
     <>
       <h1>CounterApp</h1>
       <h2> { value } </h2>
+
+      {/* <button onClick={ function (event) { console.log(event) } }> */ }
+      {/* <button onClick={ (event) => handleAdd(event) }> */ }
+      {/* Si (event) y handle(event) son iguales entonces podemos poner directamente: */ }
+      <button onClick={ handleAdd }>
+        {/* <button onClick={ (event) => handleAdd(event, 'hola') }> */ }
+        +1
+      </button>
     </>
   )
 }

f8ecae28e4a55826a0bda0891421a7b3f4058ecb
Juan Bautista Padilla <72109896+juanbpadilla@users.noreply.github.com>
2024-10-05 03:39:13 -0400

    Pruebas con async-await

--- src/base-pruebas/11-async-await.js
+++ src/base-pruebas/11-async-await.js
@@ -1,32 +1,28 @@
 
 
-// const getImagenPromesa = () => new Promise( resolve => resolve('https://ajskdhaskjdhajs.com') )
-// getImagenPromesa().then( console.log );
-
-const getImagen = async() => {
+export const getImagen = async () => {
 
     try {
 
-        const apiKey = 'C1khQe3Z7R1W2lfTO9myKeuShdqFYSGC';
-        const resp   = await fetch(`http://api.giphy.com/v1/gifs/random?api_key=${ apiKey }`);
-        const { data } = await resp.json(); 
+        const apiKey = 'tNR73FdRFrXJJ5HkR2cngTIaP4YSRos1';
+        const resp = await fetch(`http://api.giphy.com/v1/gifs/random?api_key=${apiKey}`);
+        const { data } = await resp.json();
 
         const { url } = data.images.original;
 
-        const img = document.createElement('img');
-        img.src = url;
-        document.body.append( img );
+        return url;
 
-    } catch (error) {
+    } catch ( error ) {
         // manejo del error
-        console.error(error)
+        console.error( error )
+        return 'No se encontró la imagen';
     }
-    
-    
-    
+
+
+
 }
 
- getImagen();
+getImagen();
 
 
 
